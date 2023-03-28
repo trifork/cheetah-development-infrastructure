9f47e468d801e4a9322adbc09a7654851fbacccc
peg <peg@trifork.com>
2023-03-28 11:29:04 +0200

    Setup kafka auth

--- config/kafkaconfig.sh
+++ config/kafkaconfig.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin bash
 
-echo "Waiting for Kafka to be ready..."
-cub kafka-ready -b kafka:19092 1 20 #expected_brokers timeout_seconds
+# echo "Waiting for Kafka to be ready..."
+# cub kafka-ready -b kafka:19093 1 20 #expected_brokers timeout_seconds
+
+# for user in redpanda-user; do
+bash bin/kafka-configs.sh --bootstrap-server kafka:19092 --command-config /etc/config/producer.config --alter --if-not-exists --add-config 'SCRAM-SHA-512=[password=password]' --entity-type users --entity-name redpanda
 
 echo "Creating topics with retention set to 3 years"
 for topic in OttoReadingMovingAverage OttoReading OttoReadingId JobNameInputTopic; do
-   kafka-topics --create --if-not-exists --bootstrap-server kafka:19092 --partitions 1 --replication-factor 1 --topic $topic --config retention.ms=20000
-   kafka-configs --bootstrap-server kafka:19092 --entity-type topics --entity-name $topic --alter --add-config segment.bytes=300
+   bash bin/kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:19092 --command-config /etc/config/producer.config --partitions 1 --replication-factor 1 --topic $topic --config retention.ms=20000
+   bash bin/kafka-configs.sh --bootstrap-server kafka:19092 --command-config /etc/config/producer.config --entity-type topics --entity-name $topic --alter --add-config segment.bytes=300
 done
 echo "Creating topics done"
 
@@ -38,3 +41,6 @@ echo "Creating topics done"
 #segment.jitter.ms
 #segment.ms
 #unclean.leader.election.enable
+
+# bash bin/kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:19093 --partitions 1 --replication-factor 1 --topic $topic --config retention.ms=20000 segment.bytes=300
+#    bash bin/kafka-configs.sh --alter --bootstrap-server kafka:19093 --topic $topic --config segment.bytes=300 
