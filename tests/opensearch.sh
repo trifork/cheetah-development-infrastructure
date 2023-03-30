b826d84513f629ad3f946d457bdb95072dd489f5
Casper Thygesen <cth@trifork.com>
2023-03-30 10:59:39 +0000

    update test script

--- tests/opensearch.sh
+++ tests/opensearch.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-# Only start opensearch
-docker compose --profile=oauth --profile=opensearch up --quiet-pull --force-recreate 
+set -euo pipefail
 
-# Get token
-curl -X 'POST' \
-  'http://localhost:1752/oauth2/token' \
-  -H 'accept: */*' \
-  -H 'Content-Type: application/x-www-form-urlencoded' \
-  -d 'grant_type=client_credentials&scope=&client_id=123&client_secret='
+# Only start opensearch
+#docker compose --profile=oauth --profile=opensearch up --quiet-pull --force-recreate 
+
+function get_access_token() {
+  local tenant=$1
+  local roles=$2 # backend roles
+  local response
+  local access_token
+  response=$(curl -s -X 'POST' \
+    'http://localhost:1752/oauth2/customtoken' \
+    -H 'accept: */*' \
+    -H 'Content-Type: application/json' \
+    -H 'cache-control: no-cache' \
+    -d "{
+    \"clientId\": \"$tenant\",
+    \"claims\": {
+      \"osroles\": \"$roles\",
+      \"osuser\": \"hesy\"
+    },
+    \"expiresInMinutes\": 120
+  }")
+  access_token=$(printf '%s' "$response" | jq -r '.access_token')
+  echo "$access_token"
+}
+
+TENANT=1
+service_token=$(get_access_token $TENANT 'default_service')
+admin_token=$(get_access_token $TENANT 'admin')
 
 # Test token
-curl -X GET "http://localhost:9229/_cat/indices" -H  "accept: application/json" -H  "Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjBERTI4RTU4ODI3MTA0RTdBQ0JCNDM4QzE5QTQzNjgzMjc1RjdDRjUifQ.eyJzdWIiOiIxMjMiLCJ0eXAiOiJCZWFyZXIiLCJqdGkiOiJhYjA3NzE0Zi1jMTc1LTQ2ODEtOGM0NC04MDZhNzg3MjU4NWQiLCJ2ZXIiOiIxLjAiLCJhcHB0eXBlIjoiQ29uZmlkZW50aWFsIiwiYXV0aF90aW1lIjoiMjAyMy0wMy0zMFQxMDo1MDozMS45MTM2Mzk2KzAwOjAwIiwiYXBwaWQiOiJ1cm46Y2hlZXRhaDpjbGllbnQiLCJuYmYiOjE2ODAxNzM0MzEsImV4cCI6MTY4MDE3NzAzMSwiaWF0IjoxNjgwMTczNDMxLCJpc3MiOiIxMjMiLCJhdWQiOiJodHRwczovL2NoZWV0YWhvYXV0aHNpbXVsYXRvcjoxNzUyL29hdXRoMi90b2tlbiIsImF1dGhtZXRob2QiOiJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvYXV0aGVudGljYXRpb25tZXRob2QvdGxzY2xpZW50IiwidG9waWNzIjoiKl9hbGwiLCJvc3JvbGVzIjoiZGVmYXVsdF9zZXJ2aWNlIn0.BHzYIaAfGUFHUSSOqZgQILBnlzEK82d4MK996rgTVeRt5sJUX8YuqU2f2DIN3JDGMeM-j0wcNb-gjyrMDpiZxXG1WYG1oPIrBofpLb5TohVLQbVSzL3FyzEPBOq2QTKmbVCHY98aPVS9la6jJGP_63vuLeq_f5Y2FFHtiy-i6ApFOqHgfkblTPb-HeyeHnU52Exk139wKtYLGyw4ITZmFlZHc9EsJ2fOif2NJ2Ysw-82BWpNQS9FpL5Grb57AQ04U1gqBgTLj55BNHztBjBuVvFfJzC9Md3svlpROfIYfZPEffdz7LbH_AMW10U3jyvZFGW8AlgKD9L4m6-8doigCg"
+curl -X GET "http://localhost:9229/_cat/indices" -H "Authorization: bearer $(printf '%s' "$service_token")"
+echo "OAuth2 works"
 
 # Test basic auth
 curl  -X GET "http://admin:admin@localhost:9229/_cat/indices" 
+echo "Basic auth works"
