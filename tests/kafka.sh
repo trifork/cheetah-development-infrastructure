743b5a04186ef645f6098191aa8cfcf55b5b3caf
Casper Thygesen <cth@trifork.com>
2023-04-04 19:19:19 +0000

    bad copy paste

--- tests/kafka.sh
+++ tests/kafka.sh
@@ -7,8 +7,6 @@ set -euo pipefail
 #
 
 echo "INFO - Testing if Redpanda has brokers connected:"
-
-curl --fail-with-body -XGET -H"Content-Type: application/json" http://cheetahoauthsimulator:80/.well-known/jwks.json  || exit -1
 if ! curl -s --fail-with-body -k "http://redpanda:8080/api/cluster" | jq -e -r ".clusterInfo.brokers | length > 0"; then
   echo
   echo "ERROR - Brokers not detected for RedPanda console"
