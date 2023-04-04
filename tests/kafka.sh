6265347245eea17f25a2425f63f02dc59c8d2913
Casper Thygesen <cth@trifork.com>
2023-04-04 19:20:09 +0000

    made condition more readable

--- tests/kafka.sh
+++ tests/kafka.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 #
 
 echo "INFO - Testing if Redpanda has brokers connected:"
-if ! curl -s --fail-with-body -k "http://redpanda:8080/api/cluster" | jq -e -r ".clusterInfo.brokers | length > 0"; then
+if curl -s --fail-with-body -k "http://redpanda:8080/api/cluster" | jq -e -r ".clusterInfo.brokers | length == 0"; then
   echo
   echo "ERROR - Brokers not detected for RedPanda console"
   curl -s "http://redpanda:8080/admin/startup"
