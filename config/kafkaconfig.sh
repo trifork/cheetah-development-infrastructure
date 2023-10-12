d864e178152da8649569f5866c11014a385a80a1
pegtrifork <peg@trifork.com>
2023-10-12 10:02:40 +0200

    Make initial kafka topic configurable via env

--- config/kafkaconfig.sh
+++ config/kafkaconfig.sh
@@ -4,7 +4,7 @@ echo "Creating Kafka User redpanda"
 bash bin/kafka-configs.sh --bootstrap-server kafka:19093 --alter --add-config 'SCRAM-SHA-512=[password=password]' --entity-type users --entity-name redpanda
 
 echo "Creating topics with retention set to 3 years"
-for topic in JobNameInputTopic _kafkasql-journal; do
+for topic in _kafkasql-journal $KAFKA_TOPICS; do
    bash bin/kafka-topics.sh --create --if-not-exists --bootstrap-server kafka:19093  --partitions 1 --replication-factor 1 --topic $topic --config retention.ms=94608000000
    bash bin/kafka-configs.sh --bootstrap-server kafka:19093  --entity-type topics --entity-name $topic --alter --add-config retention.ms=94608000000
 done
