803e21b1a221ba631735cab431dca4b4427ee8a0
pegtrifork <peg@trifork.com>
2024-01-05 16:13:34 +0100

    indentations for megalinter?

--- config/keycloak/keycloak_custom_role_creation.sh
+++ config/keycloak/keycloak_custom_role_creation.sh
@@ -13,7 +13,7 @@ IFS=',' read -ra roles_array <<<"$DEMO_CLIENT_ROLES"
 # Build the roles_definition_substitution string for $(env:DEMO_ROLES_DEFINITION)
 roles_definition_substitution=""
 for role in "${roles_array[@]}"; do
-      roles_definition_substitution+="{\"name\":\"$role\"},"
+    roles_definition_substitution+="{\"name\":\"$role\"},"
 done
 # Remove the trailing comma
 roles_definition_substitution="${roles_definition_substitution%,}"
@@ -21,7 +21,7 @@ roles_definition_substitution="${roles_definition_substitution%,}"
 # Build the client_roles_substitution string for $(env:DEMO_CLIENT_ROLES) using a loop
 client_roles_substitution="["
 for role in "${roles_array[@]}"; do
-      client_roles_substitution+="\"$role\","
+    client_roles_substitution+="\"$role\","
 done
 # Remove the trailing comma
 client_roles_substitution="${client_roles_substitution%,}"
@@ -29,7 +29,7 @@ client_roles_substitution+="]"
 
 # Perform substitutions and save to output file
 sed -e "s/\$(env:DEMO_ROLES_DEFINITION)/$roles_definition_substitution/g" \
-      -e "s/\$(env:DEMO_CLIENT_NAME)/$DEMO_CLIENT_NAME/g" \
-      -e "s/\$(env:DEMO_CLIENT_SECRET)/$DEMO_CLIENT_SECRET/g" \
-      -e "s/\$(env:DEMO_CLIENT_ROLES)/$client_roles_substitution/g" \
-      "$input_file" > "$output_file"
+    -e "s/\$(env:DEMO_CLIENT_NAME)/$DEMO_CLIENT_NAME/g" \
+    -e "s/\$(env:DEMO_CLIENT_SECRET)/$DEMO_CLIENT_SECRET/g" \
+    -e "s/\$(env:DEMO_CLIENT_ROLES)/$client_roles_substitution/g" \
+    "$input_file" > "$output_file"
